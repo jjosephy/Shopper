cf4055263b0851185fbf12ceae4e5cfa6febb624
jjosephy <jjosephy@hotmail.com>
2015-03-29 13:27:17 -0700

    update namespace
    Updated namespace issue that was causing app not to load

--- Nordstrom.Services.Shopper/Controllers/WalletController.cs
+++ Nordstrom.Services.Shopper/Controllers/WalletController.cs
@@ -6,11 +6,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Nordstrom.Services.Shopper.Contracts;
 
 namespace Nordstrom.Services.Shopper.Controllers
 {
     public class WalletController : ApiController
     {
+        public IHttpActionResult GetWallet(int id)
+        {
+            var wallet = new WalletV1
+            {
+                
+            };
 
+            return Ok(wallet);
+        }
     }
 }
--- Nordstrom.Services.Shopper/Global.asax.cs
+++ Nordstrom.Services.Shopper/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 
-namespace Shopper
+namespace Nordstrom.Services.Shopper
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
